a7d6a8c2a1efebf81d31d1b1d92e775fef1fe8c6
aheld <aaron.held@gopuff.com>
2020-04-19 14:28:55 -0400

    property testing left turn

--- rover.js
+++ rover.js
@@ -1,10 +1,63 @@
+const deepCopy = require('./deep_copy')
+
 function parseCommands (stringInput) {
-  const [, startingPosition] = stringInput.split('\n')
-  return {
-    startingPosition
+  const [, startingPosition, moves] = stringInput.split('\n')
+  const [x, y, facing] = startingPosition.split(' ')
+  const roverCommand = {
+    startingPosition: {
+      x,
+      y,
+      facing
+    },
+    moves: moves || []
   }
+  return roverCommand
+}
+
+// Turn Related functions
+function _processMoveAndUpdateFacing (roverCommand, newFacing) {
+  const newRoverCommand = deepCopy(roverCommand)
+  newRoverCommand.startingPosition.facing = newFacing
+  newRoverCommand.moves = newRoverCommand.moves.slice(1)
+  return newRoverCommand
+}
+
+function turnLeft (roverCommand) {
+  if (roverCommand.moves[0] !== 'L') return roverCommand
+
+  const leftTurn = new Map()
+  leftTurn.N = 'W'
+  leftTurn.W = 'S'
+  leftTurn.S = 'E'
+  leftTurn.E = 'N'
+
+  const newFacing = leftTurn[roverCommand.startingPosition.facing]
+  return _processMoveAndUpdateFacing(roverCommand, newFacing)
+}
+
+function turnRight (roverCommand) {
+  if (roverCommand.moves[0] !== 'R') return roverCommand
+
+  const rightTurn = new Map()
+  rightTurn.N = 'E'
+  rightTurn.W = 'N'
+  rightTurn.S = 'W'
+  rightTurn.E = 'S'
+
+  const newFacing = rightTurn[roverCommand.startingPosition.facing]
+  return _processMoveAndUpdateFacing(roverCommand, newFacing)
+}
+
+function formatPosition (roverCommand) {
+  const x = roverCommand.startingPosition.x
+  const y = roverCommand.startingPosition.y
+  const f = roverCommand.startingPosition.facing
+  return `${x} ${y} ${f}`
 }
 
 module.exports = function (commands) {
-  return parseCommands(commands).startingPosition
+  commands = parseCommands(commands)
+  commands = turnLeft(commands)
+  commands = turnRight(commands)
+  return formatPosition(commands)
 }
--- test/rover_test.js
+++ test/rover_test.js
@@ -15,10 +15,42 @@ describe("Rover that doesn't move", function () {
     const testRange = 100
     for (let x = 0; x < testRange; x++) {
       for (let y = 0; y < testRange; y++) {
-        for (const facing in 'N S E W'.split(' ')) {
+        'N S E W'.split(' ').forEach(facing => {
           const simpleInput = `5 5\n${x} ${y} ${facing}`
           assert.strictEqual(rover(simpleInput), `${x} ${y} ${facing}`)
-        }
+        })
+      }
+    }
+  })
+})
+
+describe('Rover that turns', function () {
+  it('should turn left for simple input', function () {
+    const noMoveInput = '5 5\n1 1 N\nL'
+    expect(rover(noMoveInput)).to.equal('1 1 W')
+  })
+  it('should turn right for simple input', function () {
+    const noMoveInput = '5 5\n1 1 N\nR'
+    expect(rover(noMoveInput)).to.equal('1 1 E')
+  })
+  it('should pass property based testing', function () {
+    this.timeout(150000)
+    // forall (f: noMoveInput) -> roverPosition) => starting point
+    const testRange = 100
+    for (let x = 0; x < testRange; x++) {
+      for (let y = 0; y < testRange; y++) {
+        ['N R E',
+          'N L W',
+          'S R W',
+          'S L E',
+          'E R S',
+          'W R N',
+          'E L N',
+          'W L S'].forEach(testcase => {
+          const [facing, turn, newFacing] = testcase.split(' ')
+          const simpleInput = `5 5\n${x} ${y} ${facing}\n${turn}`
+          assert.strictEqual(rover(simpleInput), `${x} ${y} ${newFacing}`)
+        })
       }
     }
   })
